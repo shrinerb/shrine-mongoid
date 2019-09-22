96c320c5d50b0cac4ca5f2f4d2d9481e7e95c0bc
Janko Marohnić <janko.marohnic@gmail.com>
2019-09-22 19:46:05 +0200

    Extract callback code into attacher methods

--- lib/shrine/plugins/mongoid.rb
+++ lib/shrine/plugins/mongoid.rb
@@ -26,30 +26,21 @@ class Shrine
           if shrine_class.opts[:mongoid][:validations]
             # add validation plugin integration
             model.validate do
-              next unless send(:"#{name}_attacher").respond_to?(:errors)
-
-              send(:"#{name}_attacher").errors.each do |message|
-                errors.add(name, message)
-              end
+              send(:"#{name}_attacher").send(:mongoid_validate)
             end
           end
 
           if shrine_class.opts[:mongoid][:callbacks]
             model.before_save do
-              if send(:"#{name}_attacher").changed?
-                send(:"#{name}_attacher").save
-              end
+              send(:"#{name}_attacher").send(:mongoid_before_save)
             end
 
             model.after_save do
-              if send(:"#{name}_attacher").changed?
-                send(:"#{name}_attacher").finalize
-                send(:"#{name}_attacher").persist
-              end
+              send(:"#{name}_attacher").send(:mongoid_after_save)
             end
 
             model.after_destroy do
-              send(:"#{name}_attacher").destroy_attached
+              send(:"#{name}_attacher").send(:mongoid_after_destroy)
             end
           end
 
@@ -69,6 +60,34 @@ class Shrine
       #   * Attacher#atomic_promote
       module AttacherMethods
         private
+
+        def mongoid_validate
+          return unless respond_to?(:errors)
+
+          errors.each do |message|
+            record.errors.add(name, message)
+          end
+        end
+
+        # Calls Attacher#save. Called before model save.
+        def mongoid_before_save
+          return unless changed?
+
+          save
+        end
+
+        # Finalizes attachment and persists changes. Called after model save.
+        def mongoid_after_save
+          return unless changed?
+
+          finalize
+          persist
+        end
+
+        # Deletes attached files. Called after model destroy.
+        def mongoid_after_destroy
+          destroy_attached
+        end
 
         # Saves changes to the model instance, raising exception on validation
         # errors. Used by the _persistence plugin.
